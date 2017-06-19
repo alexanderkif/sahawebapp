b9d32d1413631aa33461d4a3a20f11b5bd60f5a1
alexanderkif <alexander_kif@mail.ru>
2017-06-19 15:49:41 +0400

    add test servlet

---
+++ src/java/Servlet1.java
@@ -0,0 +1,44 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/url")
+public class Servlet1 extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        int n;
+        String str = req.getParameterMap().toString();
+        if(req.getParameter("n")==null)
+            n=1;
+        else n=Integer.valueOf(req.getParameter("n"));
+        PrintWriter out = resp.getWriter();
+        out.print("<h2>From Servlet1</h2><br>Give me \"?n= \" parameter"
+                +"<br>Method: " + req.getMethod()
+                +"<br>URL: "+ req.getRequestURL().toString()
+                +"<br>pathInfo: "+ req.getPathInfo()
+                +"<br>sessionId: "+ req.getSession().getId()
+                +"<br>parameters: "+ str
+                +"<br><br><table border=1>"
+                + IntStream.range(1, n)
+                .mapToObj(s-> "<tr>"+
+                        IntStream.range(1,10)
+                        .mapToObj(s1->"<td>"+s*s1+"</td>")
+                        .collect(Collectors.joining())
+                        +"</tr>"
+                )
+                .collect(Collectors.joining())
+                +"</table>"
+        );
+
+    }
+}
