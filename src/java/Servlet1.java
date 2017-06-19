fd52bca059ffd0a8af0fdd6a3e3ccb988a2ab03a
alexanderkif <alexander_kif@mail.ru>
2017-06-19 16:29:53 +0400

    test again

--- src/java/Servlet1.java
+++ src/java/Servlet1.java
@@ -4,23 +4,29 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet(urlPatterns = "/url")
+@WebServlet(
+        name = "Servlet1",
+        urlPatterns = {"/hello"}
+)
 public class Servlet1 extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
+
         int n;
         String str = req.getParameterMap().toString();
         if(req.getParameter("n")==null)
             n=1;
         else n=Integer.valueOf(req.getParameter("n"));
-        PrintWriter out = resp.getWriter();
-        out.print("<h2>From Servlet1</h2><br>Give me \"?n= \" parameter"
+//        PrintWriter out = resp.getWriter();
+        ServletOutputStream out = resp.getOutputStream();
+        out.write(("<h2>From Servlet1</h2><br>Give me \"?n= \" parameter"
                 +"<br>Method: " + req.getMethod()
                 +"<br>URL: "+ req.getRequestURL().toString()
                 +"<br>pathInfo: "+ req.getPathInfo()
@@ -35,8 +41,9 @@ public class Servlet1 extends HttpServlet {
                         +"</tr>"
                 )
                 .collect(Collectors.joining())
-                +"</table>"
+                +"</table>").getBytes()
         );
-
+        out.flush();
+        out.close();
     }
 }
